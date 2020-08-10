19823a2c11b7ae9b3dceb1c33f9111f93789f75d
kazuto0291 <zutozuto03170291@gmail.com>
2020-08-10 14:40:27 +0900

    ccomcmococmocmcoocmocmmocmocomcocmomcocmcomcomcomocm

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <template>
   <div id="app">
     <h1>hello</h1>
+    <Button></Button>
+    <Button></Button>
+    <Button></Button>
   </div>
 </template>
 
 <script>
+import Button from './components/Button';
 
+export default {
+  name: 'Fuga',
+  components: {
+    Button
+  }
+}
 </script>
 
 <style>
---
+++ src/components/Button.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="button">Button</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.button {
+  display: inline-block;
+  background: red;
+  border-radius: 5px;
+  color: white;
+  padding: 10px;
+  cursor: pointer;
+  margin-right: 5px;
+}
+
+.button:hover {
+  background: tomato;
+}
+
+,button:active {
+  background: yellow;
+}
+
+</style>
