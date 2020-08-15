9d26cb743e9438412500acf79a35ffab72f17936
kazuto0291 <zutozuto03170291@gmail.com>
2020-08-15 17:49:35 +0900

    components-MessageList

---
+++ src/components/Message.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="message">
+    <div class="message-data">
+      {{ date}}
+    </div>
+    <p class="message-text">
+      {{body}}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    body: {
+      type: String,
+      required: true
+    },
+    date: {
+      type: String,
+      required: true
+    },
+  }
+}
+</script>
+
+<style scoped>
+.message {
+  border-bottom: 1px solid black;
+  min-height: 80px;
+  padding: 10px;
+}
+
+.message:hover {
+  background-color: #eee;
+}
+
+.message-data {
+  font-size: 0.8em;
+  color: #777;
+  text-align: right;
+}
+
+.message-text {
+  margin: 0;
+  margin-top: 5px;
+  white-space: pre-line;
+}
+</style>
