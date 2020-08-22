7b3ae1b209eacb378496a907e81adc30d887ebe5
kazuto0291 <zutozuto03170291@gmail.com>
2020-08-22 11:29:00 +0900

    vue addrouter

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,32 @@
 <template>
-  <div>
-    <!-- <TextBox title="fuga" :onClick="click"/> -->
-    <Header />
-    <div class="app-container">
-      <SideMenu />
-      <Main />
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
     </div>
+    <router-view/>
   </div>
 </template>
 
-<script>
-import 'normalize.css';
-import Header from './components/Header'
-import SideMenu from './components/SideMenu'
-import Main from './components/Main'
-// import TextBox from './components/TextBox'
-
-export default {
-  components: {
-    Header,
-    SideMenu,
-    Main,
-    // TextBox
-  },
-  methods: {
-    click() {
-      console.log('@@@@@@@@')
-    }
-  }
-
-}
-</script>
-
 <style>
-.app-container {
-  display: flex;
-  padding-top: 60px;
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
